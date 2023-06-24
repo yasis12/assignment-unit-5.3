c77f00d6b3ba3d0d7954f71d99bd5009f0986df9
Rafael Yasis <rafaelyasis@gmail.com>
2023-06-24 11:26:31 -0500

    finished assignment.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,33 +1,68 @@
 console.log('***** Music Collection *****')
+
  let collection = [];
    
 /**
  * 
- * @param {string} title 
- * @param {string} artist 
- * @param {number} yearPublished 
- * @returns 
+ * @param {string} title tile of the album
+ * @param {string} artist artist of the album
+ * @param {number} yearPublished year album was published
+ * @returns {object} the new created album
  */
 
  function addToCollection(title, artist, yearPublished) {
-    let albumn = {
+    let album = {
         title: title,
         artist: artist,
         yearPublished: yearPublished
         };
-        collection.push(albumn);
-        return albumn
+
+        collection.push(album);
+
+        return album;
  }
 
  addToCollection('Hope', 'NF', 2023);
+ addToCollection('Journey to the Sum', 'Aries', 2020)
+
  console.log('This is my collection:',collection);
 
- function showCollection {
-    if (collection === 0){
+console.log('---- Show collection ----');
+
+ function showCollection() {
+    if (collection.length === 0){
         console.log('collection is empty');
     } else {
         for (i = 0; i < collection.length; i++) {
-            let albumn
+            
+            console.log(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}`);
         }
     }
  }
+
+console.log('--Show me the collection--');
+ showCollection();
+
+ console.log('---Find By Artist---');
+
+/**
+ * 
+ * @param {string} artist artist you are searching for
+ */
+
+
+ function findByArtist(artist) {
+
+  let artistResults = [];
+    
+    for (i = 0; i < collection.length; i++) {
+        if (collection[i].artist === artist){
+            artistResults.push(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}`);
+            return artistResults;
+        }  
+    }
+    return artistResults;
+ }
+
+ console.log(findByArtist('NF'));
+ console.log(findByArtist('Hoobastank'));
