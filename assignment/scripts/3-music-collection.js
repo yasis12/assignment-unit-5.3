4748352b9ba2c6ae019c96351056ed105a4f2135
Rafael Yasis <rafaelyasis@gmail.com>
2023-06-23 21:05:00 -0500

    did part of the assignment

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,33 @@
 console.log('***** Music Collection *****')
+ let collection = [];
+   
+/**
+ * 
+ * @param {string} title 
+ * @param {string} artist 
+ * @param {number} yearPublished 
+ * @returns 
+ */
+
+ function addToCollection(title, artist, yearPublished) {
+    let albumn = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+        };
+        collection.push(albumn);
+        return albumn
+ }
+
+ addToCollection('Hope', 'NF', 2023);
+ console.log('This is my collection:',collection);
+
+ function showCollection {
+    if (collection === 0){
+        console.log('collection is empty');
+    } else {
+        for (i = 0; i < collection.length; i++) {
+            let albumn
+        }
+    }
+ }
